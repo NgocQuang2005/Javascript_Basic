ea4a77adf8a01e260fc2dc1d5db9be55e96a442e
NgQuang05 <quang111420@gmail.com>
2025-06-06 16:34:54 +0700

    commit bài 88

--- Basic/Lesson76ToLesson100/Bai88.js
+++ Basic/Lesson76ToLesson100/Bai88.js
@@ -1,8 +1,16 @@
 //Viết chương trình JavaScript để tìm tổng tối đa có thể có của một số k số liên tiếp của nó (các số nối tiếp nhau theo thứ tự) trong một mảng số nguyên dương cho trước.
 let rsb88 = document.getElementById("rsb88");
 function b88(arr, k) {
-  let kq = [];
-  for (let i = 0; i < arr.length; i++) {}
-  //   rsb88.innerText = `Result: ${arr[k - 1]}`;
+  let maxSum = 0;
+  for (let i = 0; i < arr.length - k + 1; i++) {
+    let sum = 0;
+    for (let j = i; j < i + k; j++) {
+      sum += arr[j];
+    }
+    if (sum > maxSum) {
+      maxSum = sum;
+    }
+  }
+  rsb88.innerText = `Result: ${maxSum}`;
 }
-b88([2, 4, 5, 1, 8], 2);
+b88([2, 3, 5, 1, 6], 3);
